0a232ea08065e6c60bd1e80e9b24ce70746c3ec7
V1talquaaa <115567744+V1talquaaa@users.noreply.github.com>
2024-08-28 09:32:20 +0200

    fixed issue on mobile with swiper

--- my-app/src/app/components/History.jsx
+++ my-app/src/app/components/History.jsx
@@ -5,7 +5,7 @@ import HistorySwiper from './HistorySwiper';
 function History() {
   return (
     <section id='history' className=''>
-        <div className='container py-[102px]'>
+        <div className='container py-[60px] lg:py-[102px]'>
             <Image src='/headerLogo.png' width={371} height={89} alt='logo' className='mr-auto ml-auto' />
             <h2 className='mt-[24px] text-center text-[32px] lg:text-5xl text-mainBlack font-medium'>HISTORY</h2>
             <HistorySwiper />
