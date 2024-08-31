943b2b4207defd1601392259ec1d2b34e985e706
V1talquaaa <115567744+V1talquaaa@users.noreply.github.com>
2024-08-31 08:03:31 +0200

    issue with img on host fixed

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
     basePath: process.env.basePath ? process.env.basePath : "",
-    assetPrefix: process.env.URL? process.env.URL : undefined,
+    assetPrefix: process.env.URL ? process.env.URL : undefined,
+
+    images: {
+        unoptimized: true, 
+    },
 };
 
 export default nextConfig;
